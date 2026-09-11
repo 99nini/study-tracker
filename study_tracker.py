d0146ed4dcebb5fc6ce90af2bd337778b3819026
bunniicat <nina.nny97@gmail.com>
2026-09-11 15:14:51 +0200

    Keep CLI menu running with loop if not exited

--- study_tracker.py
+++ study_tracker.py
@@ -1,23 +1,25 @@
 # initial menu
 print('🌸 Welcome to Study Tracker!')
 
-print('\nWhat would you like to do?')
-print("1. Add a study session")
-print("2. View study sessions")
-print("3. View total study time")
-print("4. Exit")
+while True:
+    print('\nWhat would you like to do?')
+    print("1. Add a study session")
+    print("2. View study sessions")
+    print("3. View total study time")
+    print("4. Exit")
 
-choice = input('\nEnter your choice (1-4):')
-#print(f"You selected option {choice}.")
+    choice = input('\nEnter your choice (1-4):')
+    #print(f"You selected option {choice}.")
 
-#menu responses
-if choice == "1":
-    print("Let's add a study session~! 📖")
-elif choice == "2":
-    print('Here are your study sessions~ 📚')
-elif choice == "3":
-    print("Let's calculate your total study time~ ⌛")
-elif choice == "4":
-    print("Goodbye! Keep learning~ 💗")
-else:
-    print("❌ Ops! Invalid choice. Please enter a number from 1 to 4~ ❌")
+    #menu responses
+    if choice == "1":
+        print("Let's add a study session~! 📖")
+    elif choice == "2":
+        print('Here are your study sessions~ 📚')
+    elif choice == "3":
+        print("Let's calculate your total study time~ ⌛")
+    elif choice == "4":
+        print("Goodbye! Keep learning~ 💗")
+        break
+    else:
+        print("❌ Ops! Invalid choice. Please enter a number from 1 to 4~ ❌")
